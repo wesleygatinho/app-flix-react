4153c8f9e29627ff9985f07dac9af5bf3774eed7
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-06-08 19:02:04 -0300

    Ajustes inicial - em processamento...

--- src/componentes/emDestaque/emDestaque.js
+++ src/componentes/emDestaque/emDestaque.js
@@ -10,11 +10,7 @@ const EmDestaque = () => {
                 <div className='textos'>
                     <button>FRONT END</button>
                     <h1>SEO com REACT</h1>
-                    <p>Eu to aqui pra nesse vídeo dizer que a gente vai aprender a começar
-                        uma app inspirada no desenho Pokémon com Nextjs e React, ver algumas
-                        dicas sobre performance e de quebra conhecer uma plataforma sensacional
-                        pra fazer deploy que é a Vercel. Tudo em 22 minutos nesse vídeo feito
-                        com todo o carinho do mundo construindo uma "Pokedex"!</p>
+                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"!</p>
                 </div>
             </section>
         </body>

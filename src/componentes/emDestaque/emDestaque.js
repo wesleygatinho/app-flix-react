98c2a8ba1aabe492aeb8faf45c5005aea698ea53
wesleygatinho <wesley.gatinho@discente.ufma.br>
2024-06-25 00:35:42 -0300

    Atualização emDestaque1

--- src/componentes/emDestaque/emDestaque.js
+++ src/componentes/emDestaque/emDestaque.js
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
 import './emDestaque.css';
 
 const EmDestaque = () => {
+  const videos = [
+    "https://www.youtube.com/embed/izGwDsrQ1eQ?autoplay=1&loop=1&playlist=izGwDsrQ1eQ",
+    "https://www.youtube.com/embed/ulZQTrV8QlQ?autoplay=1&loop=1&playlist=ulZQTrV8QlQ",
+    "https://www.youtube.com/embed/eNvUS-6PTbs?autoplay=1&loop=1&playlist=eNvUS-6PTbs"
+  ];
+
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  const nextVideo = () => {
+    setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
+  };
+
   return (
     <section className='destaques'>
       <div className='conteudos-destaque'>
@@ -8,15 +21,15 @@ const EmDestaque = () => {
         <div className='video'>
           <iframe
             width="100%"
-            height= "100%"
-            src="https://www.youtube.com/embed/izGwDsrQ1eQ?autoplay=1&loop=1&playlist=izGwDsrQ1eQ"
+            height="100%"
+            src={videos[currentVideoIndex]}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen 
+            allowFullScreen
           ></iframe>
         </div>
         <div className='textos'>
-          <button>FRONT END</button>
+          <button onClick={nextVideo}>NEXT</button>
           <h1>SEO com REACT</h1>
           <p>
             "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
@@ -25,6 +38,6 @@ const EmDestaque = () => {
       </div>
     </section>
   );
-}
+};
 
 export default EmDestaque;
